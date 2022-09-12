fd83dbff86175b8e590a501e5252f7297d64b1bf
jialiangzouMBP <1214311807@qq.com>
2022-09-12 21:00:16 +0800

    feat:04803-medium-trim-right

--- type-challenge/04803-medium-trim-right/template.ts
+++ type-challenge/04803-medium-trim-right/template.ts
@@ -2,7 +2,10 @@
  * @Author: jialiangzou
  * @Date: 2022-09-09 22:23:02
  * @LastEditors: jialiangzou
- * @LastEditTime: 2022-09-09 22:24:16
+ * @LastEditTime: 2022-09-12 21:00:01
  * @FilePath: /ts-study/type-challenge/04803-medium-trim-right/template.ts
  */
-type TrimRight<S extends string> = any
+type Empty = " " | "\n" | "\t";
+type TrimRight<S extends string> = S extends `${infer F}${Empty}`
+  ? TrimRight<F>
+  : S;
