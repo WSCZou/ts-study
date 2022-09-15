a9a99bf515b3e2e5bf426f7cf1d60fbd8ef98006
jialiangzouMBP <1214311807@qq.com>
2022-09-15 22:18:16 +0800

    feat:05140-medium-trunc

--- type-challenge/05140-medium-trunc/test-cases.ts
+++ type-challenge/05140-medium-trunc/test-cases.ts
@@ -2,7 +2,7 @@
  * @Author: jialiangzou
  * @Date: 2022-09-14 21:59:49
  * @LastEditors: jialiangzou
- * @LastEditTime: 2022-09-14 22:00:19
+ * @LastEditTime: 2022-09-15 22:11:59
  * @FilePath: /ts-study/type-challenge/05140-medium-trunc/test-cases.ts
  */
-type Trunc = any
+type Trunc<T extends string | number> = `${T}` extends `${infer F}.${infer L}` ? F : `${T}`;
