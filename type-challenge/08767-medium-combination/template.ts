73fcc99e9d97197d67f7e4d0d1d5910ed2cf206d
jialiangzouMBP <1214311807@qq.com>
2022-09-28 22:30:32 +0800

    feat:08767-medium-combination

--- type-challenge/08767-medium-combination/template.ts
+++ type-challenge/08767-medium-combination/template.ts
@@ -2,7 +2,9 @@
  * @Author: jialiangzou
  * @Date: 2022-09-27 22:31:19
  * @LastEditors: jialiangzou
- * @LastEditTime: 2022-09-27 22:31:39
+ * @LastEditTime: 2022-09-28 22:13:14
  * @FilePath: /ts-study/type-challenge/08767-medium-combination/template.ts
  */
-type Combination<T extends string[]> = any
+type Combination<T extends string[], U = T[number], D = U> = D extends string
+  ? `${D}` | `${D} ${Combination<[], Exclude<U, D>>}`
+  : never
